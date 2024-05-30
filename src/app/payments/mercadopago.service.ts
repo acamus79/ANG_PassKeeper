c117055f1a94c8bd1195542396772d0787b44c82
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2024-05-30 19:55:50 -0300

    Update mercadopago.service.ts

--- src/app/payments/mercadopago.service.ts
+++ src/app/payments/mercadopago.service.ts
@@ -2,12 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Preference } from './payments-gateway/payments.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MercadopagoService {
-  private apiUrl = 'http://localhost:8000/payments/create_preference/'; // Asegúrate de usar la URL correcta
+  //private apiUrl = 'http://localhost:8000/payments/create_preference/'; // Asegúrate de usar la URL correcta
+  private apiUrl: string = environment.apiUrl+'/payments/create_preference/';
 
   constructor(private http: HttpClient) {}
 
