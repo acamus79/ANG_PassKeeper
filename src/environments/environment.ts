360f87ba86bda475f0b6dd969f3a2c2e025e2e90
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2024-05-30 19:55:53 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000' // Reemplazar con tu URL de la API de desarrollo
+  //apiUrl: 'http://127.0.0.1:8000' // Reemplazar con tu URL de la API de desarrollo
+  apiUrl: 'https://drf-passkeeper.onrender.com'
 };
