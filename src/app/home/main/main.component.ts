e34a7d550567134073e9149e6bb44009b4442b51
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2024-06-05 22:44:01 -0300

    main change

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent {
 
+  isLogged = false;
+  user: any;
+
+  constructor(public authService: AuthService, private router: Router) {
+    this.authService.getIsLogged().subscribe(logged => {
+      this.isLogged = logged;
+
+      if (logged) {
+        const userData = localStorage.getItem('user');
+        if (userData) {
+          this.user = JSON.parse(userData);
+        }
+      } else {
+        this.user = null;
+      }
+    });
+  }
+
+  onButtonClick() {
+    if (this.isLogged) {
+      // Redirige a la URL deseada cuando el usuario está logeado
+      this.router.navigate(['/ruta-destino']);
+    } else {
+      // Redirige a la página de login si el usuario no está logeado
+      this.router.navigate(['/login']);
+    }
+  }
 }
+
